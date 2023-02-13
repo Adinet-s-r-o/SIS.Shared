36fa1bbe2fd0760712b6cacd5a649c2267ade042
JakubSu <61908609+JakubSu@users.noreply.github.com>
2023-02-13 13:55:44 -0500

    Update provozni parametry request

--- SIS.Shared/SIS.Shared/Dto/ProvozniParametryRequest.cs
+++ SIS.Shared/SIS.Shared/Dto/ProvozniParametryRequest.cs
@@ -8,7 +8,6 @@ namespace SIS.Shared.Dto
     {
         public int ProvozniParametryId{ get; set; }
         public string DeleteU { get; set; } = String.Empty;
-
         public string? ObjektName { get; set; }
         public ProvozniParametryDto ProvozniParametry { get; set; }
         public string ConnectionString { get; set; } = String.Empty;
