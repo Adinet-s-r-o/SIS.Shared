33c2663ca8d8948bee01be87bdba33ed842b3e04
JakubSu <61908609+JakubSu@users.noreply.github.com>
2023-02-14 15:07:08 -0500

    resolves #3

--- SIS.Shared/SIS.Shared/Dto/HpvFazeResponse.cs
+++ SIS.Shared/SIS.Shared/Dto/HpvFazeResponse.cs
@@ -1,18 +1,15 @@
 ﻿using SIS.Shared.Enum;
-using SIS.Shared.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace SIS.Shared.Dto
 {
-    public class HpvFazeResponse : IExportable
+    public class HpvFazeResponse
     {
         public List<HpvFazeDto> Data { get; set; }
         public int HpvId { get; set; }
         public bool DeleteHpv { get; set; }
-        public ExportResult ExportResult { get; set; }
-        public string ExportMessage { get; set; }
         public HpvFazeResponse() 
         {
             Data = new List<HpvFazeDto>();
--- SIS.Shared/SIS.Shared/Dto/RedoxResponse.cs
+++ SIS.Shared/SIS.Shared/Dto/RedoxResponse.cs
@@ -1,18 +1,15 @@
 ﻿using SIS.Shared.Enum;
-using SIS.Shared.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace SIS.Shared.Dto
 {
-    public class RedoxResponse : IExportable
+    public class RedoxResponse 
     {
         public List<RedoxDto> Data { get; set; }
         public int RedoxId { get; set; }
         public bool DeleteRedox { get; set; }
-        public ExportResult ExportResult { get; set; }
-        public string ExportMessage { get; set; }
         public RedoxResponse()
         {
             Data = new List<RedoxDto>();
