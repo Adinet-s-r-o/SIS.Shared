9bcbfbc6ab289319e5cab55b3412d5ce40b53ff3
JakubSu <61908609+JakubSu@users.noreply.github.com>
2023-03-11 09:58:10 -0500

    Add biosensory dto, request and response

---
+++ SIS.Shared/SIS.Shared/Dto/BioSensorDto.cs
@@ -0,0 +1,28 @@
+﻿using SIS.Database.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SIS.Shared.Dto
+{
+    public class BioSensorDto
+    {
+        public int BioSensorId { get; set; }
+        public int? ObjektId { get; set; }
+        public DateTime? Mereno { get; set; }
+        public double? Bio1 { get; set; }
+        public double? Bio2 { get; set; }
+        public double? Bio3 { get; set; }
+        public double? Bio4 { get; set; }
+        public double? Bio5 { get; set; }
+        public double? Bio6 { get; set; }
+        public string? Poznamka { get; set; }
+        public string? NewU { get; set; }
+        public DateTime? NewD { get; set; }
+        public string? ModifU { get; set; }
+        public DateTime? ModifD { get; set; }
+        public string? DeleteU { get; set; }
+        public DateTime? DeleteD { get; set; }
+
+    }
+}
