58ab73926379d8ca21d660bd1fd79c7a005a449b
JakubSu <61908609+JakubSu@users.noreply.github.com>
2023-03-11 11:39:11 -0500

    Update constants and request

--- SIS.Shared/SIS.Shared/Common/Constants.cs
+++ SIS.Shared/SIS.Shared/Common/Constants.cs
@@ -79,6 +79,10 @@ namespace SIS.Shared.Common
                 //HpvFaze
                 public static readonly string BiosensoryList = ApiUrl + "biosensory/list/";
                 public static readonly string BiosensoryGetItemById = ApiUrl + "biosensory/get/";
+                public static readonly string BiosensoryGetByObjectId = ApiUrl + "biosensory/getByObjectId/";
+                public static readonly string BiosensoryGetByObjectIdAndRecordCnt = ApiUrl + "biosensory/getByObjectIdAndRecordCnt/";
+                public static readonly string BiosensoryGetByObjectIdAndFromToDate = ApiUrl + "biosensory/getByObjectIdAndFromToDate/";
+                public static readonly string BiosensoryGetByMultipleObjectIdAndFromToDate = ApiUrl + "biosensory/getByMultipleObjectIdAndFromToDate/";
                 public static readonly string BiosensoryAdd = ApiUrl + "biosensory/add/";
                 public static readonly string BiosensoryDelete = ApiUrl + "biosensory/delete/";
                 public static readonly string BiosensoryUpdate = ApiUrl + "biosensory/update/";
--- SIS.Shared/SIS.Shared/Dto/BioSensorRequest.cs
+++ SIS.Shared/SIS.Shared/Dto/BioSensorRequest.cs
@@ -10,6 +10,10 @@ namespace SIS.Shared.Dto
         public string DeleteU { get; set; } = String.Empty;
         public string ObjektName { get; set; } = String.Empty;
         public List<int?> ObjectIds { get; set; }
+        public int ObjectId { get; set; }
+        public int RecordCnt { get; set; }
+        public DateTime? DateFrom { get; set; } 
+        public DateTime? DateTo { get; set; } 
         public BioSensorDto BioSensor { get; set; }
         public string ConnectionString { get; set; } = String.Empty;
         public BioSensorRequest()
