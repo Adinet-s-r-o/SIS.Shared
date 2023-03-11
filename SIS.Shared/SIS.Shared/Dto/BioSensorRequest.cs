ea767d9f30cbb20b07becfd6d171159d01b0c4d0
JakubSu <61908609+JakubSu@users.noreply.github.com>
2023-03-11 09:58:10 -0500

    Add biosensory dto, request and response

---
+++ SIS.Shared/SIS.Shared/Dto/BioSensorRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SIS.Shared.Dto
+{
+    public class BioSensorRequest
+    {
+        public int BioSensorId { get; set; }
+        public string DeleteU { get; set; } = String.Empty;
+        public string ObjektName { get; set; } = String.Empty;
+        public BioSensorDto BioSensor { get; set; }
+        public string ConnectionString { get; set; } = String.Empty;
+        public BioSensorRequest()
+        {
+            BioSensor = new BioSensorDto();
+        }
+    }
+}
---
+++ SIS.Shared/SIS.Shared/Dto/BioSensorResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SIS.Shared.Dto
+{
+    public class BioSensorResponse
+    {
+        public List<BioSensorDto> Data { get; set; }
+        public int BioSensorId { get; set; }
+        public bool DeleteBioSensor { get; set; }
+        
+        public BioSensorResponse()
+        {
+            Data = new List<BioSensorDto>();
+        }
+    }
+}
