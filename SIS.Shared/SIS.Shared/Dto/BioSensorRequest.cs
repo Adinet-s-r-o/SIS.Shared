16b1b7b6144141dbdc268cdae63fc25f9911a007
JakubSu <61908609+JakubSu@users.noreply.github.com>
2023-03-11 11:29:52 -0500

    Modify biosensor request

--- SIS.Shared/SIS.Shared/Dto/BioSensorRequest.cs
+++ SIS.Shared/SIS.Shared/Dto/BioSensorRequest.cs
@@ -9,7 +9,7 @@ namespace SIS.Shared.Dto
         public int BioSensorId { get; set; }
         public string DeleteU { get; set; } = String.Empty;
         public string ObjektName { get; set; } = String.Empty;
-        public List<int> ObjectIds { get; set; }
+        public List<int?> ObjectIds { get; set; }
         public BioSensorDto BioSensor { get; set; }
         public string ConnectionString { get; set; } = String.Empty;
         public BioSensorRequest()
