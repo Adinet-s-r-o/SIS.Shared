8850e9ab980961507f0e1ff132ec6c885ce5dd97
JakubSu <61908609+JakubSu@users.noreply.github.com>
2023-03-13 16:01:33 -0400

    update usings

--- SIS.Shared/SIS.Shared/Dto/LocalityDto.cs
+++ SIS.Shared/SIS.Shared/Dto/LocalityDto.cs
@@ -1,9 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
-using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.ComponentModel.DataAnnotations;
+﻿using System;
 using System.Text;
 
 namespace SIS.Shared.Dto
