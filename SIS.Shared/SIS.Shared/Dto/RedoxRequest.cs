fb1b3f1d83d33e20715a8332e5760b9e6e24a01e
JakubSu <61908609+JakubSu@users.noreply.github.com>
2023-04-08 17:17:41 -0400

    update redox request

--- SIS.Shared/SIS.Shared/Dto/RedoxRequest.cs
+++ SIS.Shared/SIS.Shared/Dto/RedoxRequest.cs
@@ -8,19 +8,20 @@ namespace SIS.Shared.Dto
     {
         public int RedoxId { get; set; }
         public string DeleteU { get; set; } = String.Empty;
-        public string ObjektName { get; set; }
+        public string ObjektName { get; set; } = String.Empty;
         public int ObjectId { get; set; }
         public List<int> ObjectIds { get; set; }
         public int RecordCnt { get; set; }
-        public DateTime MeasuredDate { get; set; }
-        public DateTime DateFrom { get; set; }
-        public DateTime DateTo { get; set; }
+        public DateTime? MeasuredDate { get; set; }
+        public DateTime? DateFrom { get; set; }
+        public DateTime? DateTo { get; set; }
         public RedoxDto Redox{ get; set; }
         public string ConnectionString { get; set; } = String.Empty;
 
         public RedoxRequest() 
         {
             Redox = new RedoxDto();
+            ObjectIds = new List<int>();
         }
     }
 }
