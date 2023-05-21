6c11f36c8aa1e1864117eb312b1fb23d7f4acf48
JakubSu <61908609+JakubSu@users.noreply.github.com>
2023-05-21 21:37:54 +0200

    u

--- SIS.Shared/SIS.Shared/Dto/KampanResponse.cs
+++ SIS.Shared/SIS.Shared/Dto/KampanResponse.cs
@@ -7,7 +7,7 @@ namespace SIS.Shared.Dto
     public class KampanResponse
     {
         public List<KampanDto> Data { get; set; }
-        public int KampanId { get; set; }
+        public int? KampanId { get; set; }
         public bool IsNameUsed { get; set; }
         public bool DeleteKampan{ get; set; }
         public KampanResponse()
