8fa1199c06bcff62153ee066f590e7acaa1e1400
JakubSu <61908609+JakubSu@users.noreply.github.com>
2023-06-03 14:20:29 +0200

    update fyzchemmega

--- SIS.Shared/SIS.Shared/Dto/FyzChemMegaDto.cs
+++ SIS.Shared/SIS.Shared/Dto/FyzChemMegaDto.cs
@@ -10,16 +10,19 @@ namespace SIS.Shared.Dto
         public int? ObjektId { get; set; }
         public DateTime? Mereno { get; set; }
         public double? Hloubka { get; set; }
-        public double? Teplota1 { get; set; }
-        public double? Teplota2 { get; set; }
-        public double? Teplota3 { get; set; }
+        public double? TeplotaPh { get; set; }
+        public double? TeplotaU { get; set; }
+        public double? TeplotaCond { get; set; }
         public double? Konduktivita { get; set; }
         public double? ORP { get; set; }
         public double? Ph { get; set; }
+        public bool PlatnostAutomaticka { get; set; }
+        public bool? PlatnostManualni { get; set; }
+        public string? Poznamka { get; set; }
 
         #region NON_MODEL_ATTRIBUTES
         public double? Eh { get => ORP + 220; }
-        public double? Teplota { get => (Teplota1 + Teplota2 + Teplota3) / 3; }
+        public double? Teplota { get => (TeplotaPh + TeplotaU + TeplotaCond) / 3; }
         #endregion
         public string? NewU { get; set; }
         public DateTime? NewD { get; set; }
