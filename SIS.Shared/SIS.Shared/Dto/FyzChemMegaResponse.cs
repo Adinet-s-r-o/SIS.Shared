4e47e23586ff9e0b528a52e113365c74d93617bc
JakubSu <61908609+JakubSu@users.noreply.github.com>
2023-06-08 14:03:11 +0200

    update

--- SIS.Shared/SIS.Shared/Dto/FyzChemMegaResponse.cs
+++ SIS.Shared/SIS.Shared/Dto/FyzChemMegaResponse.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.NetworkInformation;
 using System.Text;
 
 namespace SIS.Shared.Dto
@@ -8,13 +9,15 @@ namespace SIS.Shared.Dto
     {
         public List<FyzChemMegaDto> Data { get; set; }
         public List<ObjektDto> Objekty { get; set; }
-        public List<DateTime> AbailableDates { get; set; }
+        public List<DateTime> AvailableDates { get; set; }
         public int FyzChemMegaId { get; set; }
         public bool ExistsForObjects { get; set; }
         public bool DeleteFyzChemMega { get; set; }
         public FyzChemMegaResponse()
         {
             Data = new List<FyzChemMegaDto>();
+            Objekty = new List<ObjektDto>();
+            AvailableDates = new List<DateTime>();
         }
     }
 }
