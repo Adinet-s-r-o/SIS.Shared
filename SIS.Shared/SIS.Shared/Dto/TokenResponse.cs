05c83c29b1aefb1b145fc53214386edef3750d61
JakubSu <61908609+JakubSu@users.noreply.github.com>
2024-08-01 17:45:35 -0400

    add tokenresponse

---
+++ SIS.Shared/SIS.Shared/Dto/TokenResponse.cs
@@ -0,0 +1,10 @@
+
+namespace SIS.Admin.Core.Dto
+{
+    public class TokenResponse
+    {
+        public bool Ok { get; set; }
+        public string Message { get; set; }  
+        public string DetailedMessage { get; set; }  
+    }
+}
